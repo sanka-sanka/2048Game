8b9ab866ca89d1ad02cfb802371f99bf613d7862
санька санька <vovchic53.94@mail.ru>
2020-08-12 22:55:58 +0500

    Добавьте файлы проекта.

---
+++ 2048game/test1.py
@@ -0,0 +1,13 @@
+import unittest
+from _2048game import get_number_from_index
+
+class Test2048(unittest.TestCase):
+     def test_1(self):
+        self.assertEqual(get_number_from_index(1,2), 7)
+     def test_2(self):
+        self.assertEqual(get_number_from_index(3,3), 16)
+
+if __name__ == '_2048game':
+    unittest.main()
+
+
